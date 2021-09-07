6ca63e0fc28565f57a2b3b91fb7e62f9c09d494f
rhexa <akun.kripitit@gmail.com>
2021-09-07 16:17:04 +0300

    modified script

---
+++ apt/provision.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+git clone https://github.com/rhexa/apt-apps.git
+cd apt-apps/
+chmod u+x *.sh
+
+# ./android-studio-snapd.sh
+# ./android-studio.sh
+# ./docker.sh
+# ./gcloud-cli.sh
+# ./lamp.sh
+# ./lxde.sh
+# ./lxde-mac-theme.sh
+# ./vagrant-libvirt.sh
+# ./vagrant.sh
+# ./vbox-guest-addition.sh
+# ./vscode.sh
+# ./xfce4.sh
