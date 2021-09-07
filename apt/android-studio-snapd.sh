01d87032cd55680aa3073c937464e45a90569970
rhexa <akun.kripitit@gmail.com>
2021-09-07 12:30:16 +0300

    removed apt

--- apt/android-studio-snapd.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Requires snap
-sudo snap install core
-
-# Install Android Studio
-sudo snap install android-studio --classic
--- apt/vscode.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-sudo apt install snapd
-sudo snap install core
-sudo snap install code --classic
